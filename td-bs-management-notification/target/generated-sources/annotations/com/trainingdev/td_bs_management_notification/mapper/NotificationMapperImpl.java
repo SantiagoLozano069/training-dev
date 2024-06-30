a7773dc38ff066582c234caba37b79a2d6e572c6
SantiagoLozano069 <64374377+SantiagoLozano069@users.noreply.github.com>
2024-06-30 18:52:58 -0500

    Refactor code

--- td-bs-management-notification/target/generated-sources/annotations/com/trainingdev/td_bs_management_notification/mapper/NotificationMapperImpl.java
+++ td-bs-management-notification/target/generated-sources/annotations/com/trainingdev/td_bs_management_notification/mapper/NotificationMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-06-30T17:21:23-0500",
+    date = "2024-06-30T18:46:34-0500",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.11 (Oracle Corporation)"
 )
 @Component
